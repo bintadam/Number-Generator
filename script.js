e976cf635b1cad38efff78f69e580e28df69365d
bintadam <zakiyaadan12@gmail.com>
2023-07-07 17:57:35 +0200

    add function

--- script.js
+++ script.js
@@ -10,6 +10,32 @@ for(let i of range(0, 101)){
     numbers.push(i);
 }
 
+function isPrime(n){
+    if(n<= 1){
+        return false;
+    }
+    if(n<=3){
+        return true;
+    }
+    if(n%2===0 || n%3===0){
+        return false;
+    }
+    for(let i=5; i*i <= n; i += 6){
+        if(n%i === 0 || n%(i +2) === 0){
+            return false
+        }
+    }
+    return true;
+}
+
 $(document).ready(function(){
     const card = $('.card')
+    isPrime().css()
+    for(number in numbers){
+        if(number%2 === 0){
+            $(this).css("background", "green")
+        }else if(number%2 !=0){
+            $(this).css("background", "yellow")
+        }
+    }
 })
